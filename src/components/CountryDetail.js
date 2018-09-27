245f964825bfa80061f1e6564f0c6b40fece2fe0
Josh Matos <jmatos.5@aol.com>
2018-09-27 15:24:17 -0400

    layout completed

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import FilterableProductTable from './components/FilterableProductTable';
 
 class CountryDetail extends Component {
 
   render() {
     return (
       <div className="CountryDetail">
-        <FilterableProductTable />
+        
       </div>
     );
   }
